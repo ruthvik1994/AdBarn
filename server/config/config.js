2b3d7013e1ba9cf7348d23b6f557af9c68468c55
Darshil Patel <darshil0193@gmail.com>
2018-04-03 20:00:49 -0400

    updating remote url to save db

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     dev: {
         port: process.env.PORT || 3000,
-        db: "mongodb://152.46.19.17:27017/testgridfs",
+        db: "mongodb://152.46.20.123/testgridfs",
         localDB: "mongodb://localhost:27017/testgridfs" // TODO : Remove this in the end. Only for debugging purpose
     }
-}
+};
